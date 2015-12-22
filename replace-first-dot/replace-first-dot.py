847b62e47ec6f559e0312a96b767149dc67af650
shrinivasan <tshrinivasan@gmail.com>
2015-12-22 15:19:45 +0530

    Fixed the replace function

--- replace-first-dot/replace-first-dot.py
+++ replace-first-dot/replace-first-dot.py
@@ -1,36 +1,62 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import re
+
+#Based on https://gist.github.com/bravegnu/03acfa2473c97911d252
+#Thanks to Vijayakumar vijaykumar@zilogic.com  of ChennaiPy Group.
+
+
+STATE_SEARCH_DOT = 0
+STATE_SEARCH_CLOSE_FLOWER = 1
+STATE_SEARCH_CLOSE_PAREN = 2
+
+matching = {
+    '{': '}',
+    '(': ')'
+}
+
 input_file = raw_input("Enter the input file to replace first occurance: ")
 output_file = raw_input("Enter the name of the output file: ")
 
-find = "."
-replace = "#:{{எ.கா}}"
-
-inputs = open(input_file,'r').readlines()
-
 outputs = open(output_file,'w')
 
-for line in inputs:
-    if "." in line:
-#        print line
-        if re.search('{.*\.*}', str(line)):
-            bracket = re.search('{.*\.*}', str(line)).group(0)
-            word_before_dot = line.split(".")[0].split(' ')[-1]
-            if word_before_dot in bracket:
-                outputs.write(line)
-            elif word_before_dot in bracket:
-                #print word_before_dot
-                line = line.replace(find, replace, 1)
-                #The third parameter is the maximum number of occurrences that you want to replace
-                outputs.write(line)
-            else:
-                outputs.write(line)
-    else:
-        outputs.write(line)
+find_string = "."
+replace_string = "#:{{எ.கா}}"
 
+
+
+def find_first_dot(line):
+    paren_stack = []
+
+    for pos, ch in enumerate(line):
+        if ch in matching:
+            paren_stack.append(ch)
+
+        else:
+            if paren_stack: 
+                if ch == matching[paren_stack[-1]]:
+                    paren_stack.pop()
+            else:
+                if ch == find_string:
+                    return pos
+
+    return -1
+
+            
+for line in open(input_file):
+#    print line
+    outputs.write(line)
+    
+    pos = find_first_dot(line)
+    if pos != -1:
+        line = list(line)
+        line[pos] = replace_string
+        line = "".join(line)
+
+#    print line
+    outputs.write(line)
+
+
+print "The output is written to : " + output_file
 outputs.close()
 
-print "Find and replace completed"
-    
