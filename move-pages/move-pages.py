caaf00a3985078a7cbc958fc04db06dde63ac947
shrinivasan <tshrinivasan@gmail.com>
2016-06-08 01:10:48 +0530

    added move pages

---
+++ move-pages/move-pages.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+
+import os
+import shutil
+import sys
+import ConfigParser
+import time
+import datetime
+import glob
+import urllib
+import logging
+import urllib2
+import wikitools
+import poster
+
+config = ConfigParser.ConfigParser()
+config.read("config.ini")
+
+wiki_username = config.get('settings','wiki_username')
+wiki_password = config.get('settings','wiki_password')
+wikisource_language = config.get('settings','wikisource_language')
+
+wiki_url = "https://" + wikisource_language + ".wikisource.org/w/api.php"
+
+original_book_name = config.get('settings','original_book_name')
+new_book_name = config.get('settings','new_book_name')
+start_page_number = config.get('settings','start_page_number')
+end_page_number = config.get('settings', 'end_page_number')
+
+
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+ts = time.time()
+timestamp  = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d-%H-%M-%S')
+
+
+if not os.path.isdir("./log"):
+            os.mkdir("./log")
+
+
+
+# create a file handler
+log_file = './log/add_license_' + timestamp + '_log.txt'
+
+handler = logging.FileHandler(log_file)
+handler.setLevel(logging.INFO)
+
+# create a logging format
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+# add the handlers to the logger
+
+logger.addHandler(handler)
+
+
+try:
+            wiki = wikitools.wiki.Wiki(wiki_url)
+except:
+            message =  "Can not connect with wiki. Check the URL"
+            logger.info(message)
+
+login_result = wiki.login(username=wiki_username,password=wiki_password)
+message =  "Login Status = " + str(login_result)
+logging.info(message)
+
+
+if login_result == True:
+        message =  "\n\nLogged in to "  +  wiki_url.split('/w')[0]
+        logging.info(message)
+else:
+        message =  "Invalid username or password error"
+        logging.info(message)
+        sys.exit()
+
+def check_for_bot(username):
+        user = wikitools.User(wiki,username)
+        if 'bot' in user.groups:
+                return "True"
+
+logging.info("Checking for bot access rights")
+bot_flag = check_for_bot(wiki_username)
+
+if bot_flag:
+            logging.info("The user " + wiki_username + " has bot access.")
+else:
+            logging.info("The user " + wiki_username + " does not have bot access")
+
+
+def move_page(original_pagename, new_pagename):
+
+	page = wikitools.Page(wiki, original_pagename, followRedir=True)
+        
+	logging.info("Editing " + "https://ta.wikisource.org/wiki/"+page.title)
+
+	page.move( new_pagename, reason = u"விக்கிக்கு ஏற்ப பெயர் மாற்றப் பட்டது", noredirect=True)
+
+#	page.edit(text = new_content,summary = "உரிமப்பக்கத்திற்குரிய தரவைப் பதிவேற்றியது")
+	
+#	print page.getWikiText()
+
+        logging.info("moved " +  original_pagename + " to " + new_pagename )
+        
+
+
+
+
+counter = 1
+
+for number in range(int(start_page_number), int(end_page_number) +1 ):
+
+	original_name = original_book_name + "/" + str(number)
+	new_name = new_book_name + "/" + str(number)
+
+	logging.info("Moving page " + str(counter))
+	move_page(original_name, new_name)
+
+	
+        counter = counter + 1
+        
+	sys.exit()
+
+        time.sleep(8)
+
+
+logging.info("Completed!")
