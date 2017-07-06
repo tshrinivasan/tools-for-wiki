9b3cebd5365aad6cd5c236912d20a6086c128aa4
shrinivasan <shrinivasan@netcalyx.co>
2017-07-07 02:21:55 +0530

    added total and links

--- article-counts-in-category/count_articles.py
+++ article-counts-in-category/count_articles.py
@@ -9,7 +9,7 @@ import os
 from time import gmtime, strftime
 
 site = wiki.Wiki("https://ta.wikipedia.org/w/api.php") 
-#site.login("TshrinivasanBOT", "Vanakkam@1")
+site.login("TshrinivasanBOT", "Palagai@1")
 # Create object for "Category:Foo"
 
 timestamp = strftime("%Y-%m-%d %H:%M:%S", gmtime())
@@ -25,20 +25,31 @@ def get_count(district):
 	return counter
 
 
-total_time = open('./csv-to-html-table/data/time_total.html','w')
 
-total_time.write('<link href="../css/bootstrap.min.css" rel="stylesheet">\n')
-total_time.write("<p align='right'> இந்தப் பட்டியல் 10 நிமிடத்திற்கு ஒரு முறை இற்றைப்படுத்தப்படுகிறது. கடைசி இற்றை நேரம்   " + timestamp + "<br/>")
-total_time.close()
 
 districts = open('./districts.txt','r')
 
 articles_count = open("./csv-to-html-table/data/articles_count.csv",'w')
-articles_count.write("மாவட்டம் , கட்டுரைகள்" +"\n")
+articles_count.write("மாவட்டம் ~ கட்டுரைகள்" +"\n")
 
 for district in districts:
-	content = district.strip("\n") + " , " + str(get_count(district)) + "\n"
+	content = "<a target='_blank' href='https://ta.wikipedia.org/wiki/பகுப்பு:" + district.strip("\n") + " மாவட்ட ஆசிரியர்கள் தொடங்கிய கட்டுரைகள்'>" + district.strip("\n") + "</a>" + " ~ " + str(get_count(district)) + "\n"
 	print content
 	articles_count.write(content)
 
+articles_count.close()
+
+
+with open("./csv-to-html-table/data/articles_count.csv") as fin:
+    headerline = fin.next()
+    total = 0
+    for row in csv.reader(fin,delimiter='~'):
+	total += int(row[1])
+
+total_time = open('./csv-to-html-table/data/time_total.html','w')
+
+total_time.write('<link href="../css/bootstrap.min.css" rel="stylesheet">\n')
+total_time.write("<p align='right'> இந்தப் பட்டியல் 10 நிமிடத்திற்கு ஒரு முறை இற்றைப்படுத்தப்படுகிறது. கடைசி இற்றை நேரம்   " + timestamp + "<br/>")
+total_time.write(" மொத்தக் கட்டுரைகள் =   " + str("{:,}".format(total)) + "</p>")
+total_time.close()
 
