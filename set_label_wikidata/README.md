675bede722e0113cfe9569ca15315b118a0b78de
shrinivasan <shrinivasan@netcalyx.co>
2018-08-17 15:32:07 +0530

    modified for python 3

--- set_label_wikidata/README.md
+++ set_label_wikidata/README.md
@@ -1,6 +1,7 @@
 # INSTALL
 
-sudo pip install wikidataintegrator
+sudo pip3 install wikidataintegrator
+sudo pip3 install tqdm
 
 run the above command to install the python module wikidataintegrator.
 
@@ -9,10 +10,21 @@ run the above command to install the python module wikidataintegrator.
 
 The script set_label_in_wikidata.py is used to set the label in desired language for the items defined in a CSV file.
 
-The file data.csv should have the item id and label in your language. The should be seperated by ~ .
+The file data.csv should have the item id and label in your language and alias. The should be seperated by ~ .
 
 Then, edit the file set_label_in_wikidata.py and set the parameters wikidata_username and wikidata_password.
 
 Then, run it via
 
-python set_label_in_wikidata.py
+python3 set_label_in_wikidata.py
+
+
+# Sample data
+
+Q9170~கோசி மண்டலம்~
+Q9172~லும்பினி மண்டலம்~test
+
+
+# Note
+
+The data.csv should have 3 columns. item id, label, and alias. If you have alias, end the line with Empty ~ as in sample line 1
--- set_label_wikidata/set_label_in_wikidata.py
+++ set_label_wikidata/set_label_in_wikidata.py
@@ -8,23 +8,19 @@ import time
 
 input_file = "data.csv"
 language_code = 'ta'
-wikidata_username = 'USERNAME'
-wikidata_password = 'PASSWORD'
+wikidata_username = 'Tshrinivasan'
+wikidata_password = 'Vanakkam@1'
 
 login_session = wdi_login.WDLogin(user=wikidata_username, pwd=wikidata_password)
 
 
 def set_new_label(item_id,label,language_code,alias_text):
 
-	wikidata_item = wdi_core.WDItemEngine(wd_item_id=item_id)
-
-	wikidata_item.set_label(label, lang=language_code)
-
+        wikidata_item = wdi_core.WDItemEngine(wd_item_id=item_id)
+        wikidata_item.set_label(label, lang=language_code)
         wikidata_item.set_aliases(alias_text, lang=language_code, append=True)
-
-	wikidata_item.write(login_session,edit_summary='added label in ' + language_code + " language")
-
-	print "Wrote the new label. check the link " + 'https://www.wikidata.org/wiki/' + item_id + "\n"
+        wikidata_item.write(login_session,edit_summary='added label in ' + language_code + " language")
+        print("Wrote the new label. check the link " + 'https://www.wikidata.org/wiki/' + item_id + "\n")
 
 
 
@@ -35,7 +31,7 @@ for row in reader:
 		set_new_label(row[0],row[1],language_code, row[2])
 		time.sleep(5)
 	except Exception as e:
-		print "Error on " + row[0] 
-		print  str(e)
-		print "\n"
+		print("Error on " + row[0]) 
+		print(str(e))
+		print("\n")
 
