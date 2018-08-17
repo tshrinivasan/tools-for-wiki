7be9a92060f84dc074f660e8fd752e0cb1e67eb5
shrinivasan <shrinivasan@netcalyx.co>
2018-08-17 13:18:33 +0530

    added alias

--- set_label_wikidata/set_label_in_wikidata.py
+++ set_label_wikidata/set_label_in_wikidata.py
@@ -14,11 +14,13 @@ wikidata_password = 'PASSWORD'
 login_session = wdi_login.WDLogin(user=wikidata_username, pwd=wikidata_password)
 
 
-def set_new_label(item_id,label,language_code):
+def set_new_label(item_id,label,language_code,alias_text):
 
 	wikidata_item = wdi_core.WDItemEngine(wd_item_id=item_id)
 
 	wikidata_item.set_label(label, lang=language_code)
+
+        wikidata_item.set_aliases(alias_text, lang=language_code, append=True)
 
 	wikidata_item.write(login_session,edit_summary='added label in ' + language_code + " language")
 
@@ -30,7 +32,7 @@ f = open(input_file, 'r')
 reader = csv.reader(f, delimiter='~')
 for row in reader:
 	try:
-		set_new_label(row[0],row[1],language_code)
+		set_new_label(row[0],row[1],language_code, row[2])
 		time.sleep(5)
 	except Exception as e:
 		print "Error on " + row[0] 
